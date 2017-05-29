9e5aeaddc472cbae2e13567969766be4f7a67d6f
Connor Goggins <cgoggins@wustl.edu>
2017-05-28 23:42:26 -0700

    Using python script to download mp3

---
+++ parse-mp3.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('/Users/connorgoggins/Library/Caches/pip/http')
+import youtube_dl
+import os
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+ydl_opts = {
+    'format': 'bestaudio[ext=m4a]/best[ext=mp4]/best',
+    'outtmpl': os.path.abspath("songs") + '/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+    'logger': MyLogger(),
+    'progress_hooks': [my_hook],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=hBA0PUQCvIA'])
+
+#https://www.youtube.com/watch?v=I5h8GfxIWVY
