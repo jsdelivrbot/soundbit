4cac15566a73d495fbe955fb2c891a9cc8c0e8b2
Connor Goggins <cgoggins@wustl.edu>
2017-06-28 18:43:17 -0400

    Trying to add metadata to audio files

--- server.js
+++ server.js
@@ -361,7 +361,7 @@ app.get('/downloadSong', function (req, res) {
 
   var video = youtubedl('http://www.youtube.com/watch?v=' + videoId,
     // Optional arguments passed to youtube-dl.
-    ['--format=bestaudio', '--embed-thumbnail', '--write-thumbnail', '--add-metadata'],
+    ['--format=best', '--embed-thumbnail', '--write-thumbnail', '--add-metadata'],
     // Additional options can be given for calling `child_process.execFile()`.
     { cwd: __dirname });
 
